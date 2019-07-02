b1631407d65921852f2dd5769454248d6a26cb3c
goldgust <crystalfyre100@gmail.com>
2019-07-02 20:49:25 +0000

    "

---
+++ src/list.ts
@@ -0,0 +1,20 @@
+export class List {
+    /*
+    Should be able to
+    -create with length
+    -return length
+    -access element (input and return contents)
+    -add element to end
+    -concatenate two lists
+    */
+
+    /*Structure: a head with a chain of elements I guess*/
+
+    constructor(length: int<?) {
+        this.length = length;
+    }
+
+    add(a:number, b:number):number {
+        return a+b
+    }
+}
